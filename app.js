753365a798ca4176a9395f0b4966e2a904f97cc1
Rohan <rohanduaSP@bitbucket.org>
2021-01-08 21:38:38 +0530

    included the interceptor in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 8889;
 const bodyParser = require('body-parser');
 const accessLogger = require('./middleware/accessLogger')
 const addMetaData = require('./middleware/addMetaData')
+require('./middleware/interceptor').addRequestId(); 
 var app = express();
 
 app.use(bodyParser.json({ type: 'application/json', limit: '50mb' }));
